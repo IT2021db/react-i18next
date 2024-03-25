dc66a5029f804c8b12bbbadfe0852446867acabb
Dorota Borowska <youcode2021db@gmail.com>
2024-03-25 20:49:09 +0100

    Feature: opportunity for language choice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-// import {  Trans } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  pl: { nativeName: 'Polish' }
+};
 
 function App() {
-  // const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (<button key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>
+            {lngs[lng].nativeName}
+             </button>))}
+        </div>
         <p>
-           {/* Użyj tłumaczenia z klucza 'description.part1' */}
-          {/* <Trans i18nKey="description.part1">
+          <Trans i18nKey="description">
             Edit <code>src/App.js</code> and save to reload.
-          </Trans>        */}
-           </p>
+          </Trans>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-           {/* Użyj tłumaczenia z klucza 'description.part2' */}
-          {/* {t('description.part2')} */}
+          {t('learn')}
         </a>
       </header>
     </div>
